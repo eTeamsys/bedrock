9429188810f83fe3c6b3d2fe9ff75ac25fa957c3
Christophe GARCIA <christophe@eteamsys.lu>
2019-10-30 16:26:50 +0100

    update deploy library

--- scripts/install_uploads.sh
+++ scripts/install_uploads.sh
@@ -1,4 +1,7 @@
 # !/bin/bash
 
- ln -s /media/efs $(pwd)/web/app/uploads
+echo "$(pwd)/web/app/uploads"
 
+ ln -s /media/efs $(pwd)/web/app/uploads
+chown -R www-data:www-data $(pwd)/web/app/uploads
+chmod -R 775  $(pwd)/web/app/uploads
