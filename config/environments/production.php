2c60578e62d08293b76c0291e06414c16ede2809
Christophe GARCIA <christophe@eteamsys.lu>
2019-10-30 15:54:21 +0100

    add all production dependencies

--- config/environments/production.php
+++ config/environments/production.php
@@ -7,9 +7,11 @@ if ($_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
 }
 
 ini_set('display_errors', 0);
+
+define('ICL_DONT_LOAD_LANGUAGES_JS', true);
+
 Config::define('WP_DEBUG_DISPLAY', false);
 Config::define('SCRIPT_DEBUG', false);
-Config::define('ICL_DONT_LOAD_LANGUAGES_JS', true);
 Config::define( 'WP_MEMORY_LIMIT', '128M' );
 Config::define( 'WP_MAX_MEMORY_LIMIT', '256M' );
 Config::define('WP_DEBUG', false);
