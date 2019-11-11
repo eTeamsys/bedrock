6ef38b04ba6936ef1a454e34bfced07e09f1d50f
Christophe GARCIA <christophe@eteamsys.lu>
2019-11-11 10:42:32 +0100

    fix script ln

--- scripts/install_uploads.sh
+++ scripts/install_uploads.sh
@@ -2,6 +2,6 @@
 
 echo "$(pwd)/web/app/uploads"
 
- ln -s -T /media/efs/ $(pwd)/web/app/uploads/
+ln -s -T /media/efs/ $(pwd)/web/app/uploads/
 chown -R www-data:www-data $(pwd)/web/app/uploads
-chmod -R775  $(pwd)/web/app/uploads
+chmod -R 775  $(pwd)/web/app/uploads
