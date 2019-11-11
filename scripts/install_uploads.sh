ae87d3b2ba7768f50bc3d18d9af68c7c2f4b3fa5
Christophe GARCIA <christophe@eteamsys.lu>
2019-11-11 10:44:52 +0100

    remove R chmod

--- scripts/install_uploads.sh
+++ scripts/install_uploads.sh
@@ -4,4 +4,4 @@ echo "$(pwd)/web/app/uploads"
 
 ln -s -T /media/efs/ $(pwd)/web/app/uploads/
 chown -R www-data:www-data $(pwd)/web/app/uploads
-chmod -R 775  $(pwd)/web/app/uploads
+chmod 775  $(pwd)/web/app/uploads
