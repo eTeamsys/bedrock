bffe6415950e374067dc15702553bd2757077729
Christophe GARCIA <christophe@eteamsys.lu>
2019-11-13 12:04:32 +0100

    staging version

--- scripts/install_uploads.sh
+++ scripts/install_uploads.sh
@@ -1,7 +1,8 @@
 # !/bin/bash
 
+echo "../uploads/"
 echo "$(pwd)/web/app/uploads"
 
-ln -s -T /media/efs/ $(pwd)/web/app/uploads/
+ln -s -T ../uploads/ $(pwd)/web/app/uploads/
 chown -R www-data:www-data $(pwd)/web/app/uploads
 chmod 775  $(pwd)/web/app/uploads
