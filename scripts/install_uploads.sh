f9e38a076effe31cc2b557eb6b9e9944de65fa6b
Christophe GARCIA <christophe@eteamsys.lu>
2019-11-13 12:25:25 +0100

    modify script install symlink

--- scripts/install_uploads.sh
+++ scripts/install_uploads.sh
@@ -1,8 +1,8 @@
 # !/bin/bash
 
-echo "../uploads/"
+echo "$(pwd)/../uploads/"
 echo "$(pwd)/web/app/uploads"
 
-ln -s -T ../uploads/ $(pwd)/web/app/uploads/
+ln -s -T $(pwd)/../uploads/ $(pwd)/web/app/uploads/
 chown -R www-data:www-data $(pwd)/web/app/uploads
 chmod 775  $(pwd)/web/app/uploads
