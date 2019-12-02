a64d6865eab5cbcbd25aa361d5cfdab57bd7065a
Romain Reuchamps <romain@eteamsys.lu>
2019-12-02 11:39:23 +0100

    Add command line to restart apache

--- scripts/install_uploads.sh
+++ scripts/install_uploads.sh
@@ -5,3 +5,5 @@ echo "$(pwd)/web/app/uploads"
 ln -s -T /media/efs/ $(pwd)/web/app/uploads/
 chown -R www-data:www-data $(pwd)/web/app/uploads
 chmod 775 $(pwd)/web/app/uploads
+
+service php7.2-fpm restart && service apache2 reload
