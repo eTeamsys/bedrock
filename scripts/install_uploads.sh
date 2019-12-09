c88dc392f065ebb518b2c7090a25041813ed4ada
Romain Reuchamps <romain@eteamsys.lu>
2019-12-09 14:26:26 +0100

    Edit theme and install script

--- scripts/install_uploads.sh
+++ scripts/install_uploads.sh
@@ -1,9 +1,14 @@
 # !/bin/bash
 
-echo "$(pwd)/web/app/uploads"
+UPLOAD=$(pwd)/web/app/uploads
 
-ln -s -T /media/efs/ $(pwd)/web/app/uploads/
-chown -R www-data:www-data $(pwd)/web/app/uploads
-chmod 775 $(pwd)/web/app/uploads
+if [ ! -L "$UPLOAD" ]; then
+    echo "create $UPLOAD symlink"
+    ln -s -T /media/efs/ $UPLOAD
+    chown -R www-data:www-data $UPLOAD
+    chmod 775  $UPLOAD
+else
+    echo "$UPLOAD already exists"
+fi
 
-service php7.2-fpm restart && service apache2 reload
+rm -Rf /var/cache/mod_pagespeed/* && php -r "opcache_reset();" && service php7.2-fpm reload && rm -Rf app/cache/cache-enabler/*
